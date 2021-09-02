cf3a5628f4ad8446fda40c73f08a90d4e53482a0
Hanamant1999 <hanamantbandivaddar.xworkz@gmail.com>
2021-09-03 00:05:33 +0530

    Tasks on Method

---
+++ AddTwoNumbers.java
@@ -0,0 +1,18 @@
+class AddTwoNumbers{
+
+	public static void main(String a[]){
+
+	  add(525,452);
+	  add(855,542);
+	  add(855,452);
+	  add(455,652);
+	  add(525,653);
+	}
+
+
+	public static void add(int a,int b){
+
+		System.out.println(a+b+c);
+	}
+
+}
---
+++ DivisionOfTwoNumbers.java
@@ -0,0 +1,17 @@
+class DivisionOfTwoNumbers{
+
+	public static void main(String a[]){
+	
+		division(45,2);
+		division(60,2);
+		division(88,2);
+		division(66,2);
+		division(50,2);
+		}
+	
+	public static void division(int a,int b){
+	
+	
+	System.out.println(a/b);
+	}
+}
