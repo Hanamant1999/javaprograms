726826c40a96423ad9c16544d3081729ec0df29c
Hanamant1999 <hanamantbandivaddar.xworkz@gmail.com>
2021-10-04 22:05:11 +0530

    programs

---
+++ EngineerTester.java
@@ -0,0 +1,37 @@
+class EngineerTester {
+	
+	public static void main(String a[]){
+		
+		System.out.println("-------------------------------------------------------");
+		Engineer ece =new Ece();
+		
+		System.out.println(ece.toString());
+		
+		System.out.println("-------------------------------------------------------");
+		
+		Engineer cse =new Cse();
+		
+		System.out.println(cse.toString());
+		
+		System.out.println("-------------------------------------------------------");
+		
+		Engineer mech =new Mech();
+		
+		System.out.println(mech.toString());
+		
+		System.out.println("-------------------------------------------------------");
+		
+		
+		Engineer civil =new Civil();
+		
+		System.out.println(civil.toString());
+		
+		System.out.println("-------------------------------------------------------");
+		
+		Engineer eee =new Electrical ();
+		
+		System.out.println(eee.toString());
+		
+		System.out.println("-------------------------------------------------------");
+	}
+}
